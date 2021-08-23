b426d2208c33d559960ded3f3b7fea369a9f6308
Olga <tokostyle19@gmail.com>
2021-08-23 10:54:32 +0300

    first commit

---
+++ admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://iamtokoloc/admin/');
+define('HTTP_CATALOG', 'http://iamtokoloc/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://iamtokoloc/admin/');
+define('HTTPS_CATALOG', 'http://iamtokoloc/');
+
+// DIR
+define('DIR_APPLICATION', 'E:/OpenServer/domains/iamtokoloc/admin/');
+define('DIR_SYSTEM', 'E:/OpenServer/domains/iamtokoloc/system/');
+define('DIR_IMAGE', 'E:/OpenServer/domains/iamtokoloc/image/');
+define('DIR_STORAGE', 'E:/OpenServer/domains/storage/');
+define('DIR_CATALOG', 'E:/OpenServer/domains/iamtokoloc/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'root');
+define('DB_DATABASE', 'iamtokoloc');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
---
+++ system/storage/cache/a6/a69c87e05dbe3faa50f035a5475e4e4305895c89ec1daa4e802f80abad3b5054.php
@@ -0,0 +1,50 @@
+<?php
+
+/* common/footer.twig */
+class __TwigTemplate_ad9654770bec3f33f56bf7dab9744f4d3d45bc26556c013196baeff194b96cbc extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "<footer>
+  <div class=\"container\">
+    <a href=\"http://opencart.ru\" target=\"_blank\">";
+        // line 3
+        echo (isset($context["text_project"]) ? $context["text_project"] : null);
+        echo "</a>
+  </div>
+</footer>
+</body></html>";
+    }
+
+    public function getTemplateName()
+    {
+        return "common/footer.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  23 => 3,  19 => 1,);
+    }
+}
+/* <footer>*/
+/*   <div class="container">*/
+/*     <a href="http://opencart.ru" target="_blank">{{ text_project }}</a>*/
+/*   </div>*/
+/* </footer>*/
+/* </body></html>*/
